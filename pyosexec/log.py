91791546b7f4fa95e9bc1b2c02297df32bd68046
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-04 07:08:14 -0600

    Create log.py
    Logger used by project in a single file

---
+++ pyosexec/log.py
@@ -0,0 +1,18 @@
+import logging
+
+
+def set_logger(new_logger):
+    global logger
+    logger = new_logger
+
+
+def disable_logger():
+    logger.setLevel(logging.WARNING)
+
+
+def enable_logger(level):
+    logger.setLevel(level)
+
+
+logger = logging.getLogger("pyosexec")
+logger.setLevel(logging.INFO)
