be2494e1753f5e7893e7414e56da0f8f79aff8d1
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-06 22:08:10 -0600

    Create setup.py
    TODO: rename pyosexec

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="pyosexec",
+    version="0.0.0",
+    license="None",
+    author="Jayesh Joshi",
+    author_email="jayeshjo1@utexas.edu",
+    description="Simple local and remote executor",
+    packages=["pyosexec"],
+    install_requires=["pyzmq"]
+)
