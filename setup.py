3df2a18baf42626a0a549d2e70aee2de4d5ba45e
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-10 23:53:04 -0600

    Add command line scripts
    Added click as required library (for command line parsing).
    Scripts:
    * install = installs scripts to run on startup
    * start_server = starts the server with listening

--- setup.py
+++ setup.py
@@ -3,12 +3,19 @@ from setuptools import setup
 
 setup(
     name="conplyent",
-    version="0.0.0",
+    version="0.0.1",
     license="MIT",
     author="Jayesh Joshi",
     author_email="jayeshjo1@utexas.edu",
     url="https://github.com/joshijayesh/conplyent/",
     description="Local and Remote Console executor",
     packages=["conplyent"],
-    install_requires=["pyzmq>=17.1.2"],
+    install_requires=[
+        "pyzmq>=17.1.2",
+        "click>=7.0"],
+    entry_points={
+        "console_scripts": [
+            "conplyent=scripts.cli:cli"
+        ],
+    },
 )
