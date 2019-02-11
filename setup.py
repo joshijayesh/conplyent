ae9d0c985e918ac50106edfb69f6a644aaa27134
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-10 23:51:05 -0600

    Revert "Add command line scripts"
    This reverts commit 45e0faf4ac9c2405d3ed139bf1c320cbcd3f69bf.

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@ from setuptools import setup
 
 setup(
     name="conplyent",
-    version="0.0.1.dev",
+    version="0.0.0",
     license="MIT",
     author="Jayesh Joshi",
     author_email="jayeshjo1@utexas.edu",
     url="https://github.com/joshijayesh/conplyent/",
     description="Local and Remote Console executor",
     packages=["conplyent"],
-    install_requires=[
-        "pyzmq>=17.1.2",
-        "click>=7.0"],
-    entry_points={
-        "console_scripts": [
-            "conplyent=scripts.cli:cli"
-        ],
-    },
+    install_requires=["pyzmq>=17.1.2"],
 )
