58bf2b62ec7c32c33d2359793c3ae522a4c03a7a
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-14 21:53:48 -0600

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="conplyent",
-    version="0.0.5",
+    version="0.0.6",
     license="MIT",
     author="Jayesh Joshi",
     author_email="jayeshjo1@utexas.edu",
