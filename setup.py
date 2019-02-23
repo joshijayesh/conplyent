a8b1f89742b097d1bcc3ec019ea8c56fe3a4b397
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-23 11:49:03 -0600

    Move from alpha => beta
    Beta relase 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="conplyent",
-    version="0.0.12",
+    version="0.1.0",
     license="MIT",
     author="Jayesh Joshi",
     author_email="jayeshjo1@utexas.edu",
