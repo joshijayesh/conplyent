3253d8bb75b911d057afb72a22755b00cf770360
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-22 20:05:01 -0600

    Update test_client for transfer

--- tests/test_client.py
+++ tests/test_client.py
@@ -30,21 +30,23 @@ class TestSimpleCommands(TestClient):
         self._client_connection.connect()
         server_commands = self._client_connection.server_methods()
         for command in server_commands:
+            if(command == 'transfer'):
+                continue
             self.assertIn(command, conplyent.MSG_PORT.keys())
 
     def test_cd_cwd(self):
         self._client_connection.connect()
         output = self._client_connection.cd("..")
         os.chdir("..")
-        self.assertEqual(output[1], os.getcwd())
+        self.assertEqual(output[0], os.getcwd())
         output = self._client_connection.cd("tests")
         os.chdir("tests")
-        self.assertEqual(output[1], os.getcwd())
+        self.assertEqual(output[0], os.getcwd())
 
     def test_ls(self):
         self._client_connection.connect()
         output = self._client_connection.ls()
-        self.assertIn("f test_client.py", output[1])
+        self.assertIn("f test_client.py", output[0])
 
 
 if(__name__ == "__main__"):
