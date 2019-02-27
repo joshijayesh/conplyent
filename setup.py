a0b7674be8799ef2fc70dd31a424d5738d213419
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-26 22:49:15 -0600

    commit v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="conplyent",
-    version="0.1.0",
+    version="0.1.2",
     license="MIT",
     author="Jayesh Joshi",
     author_email="jayeshjo1@utexas.edu",
