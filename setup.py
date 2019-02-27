708caf7907501eaf0b4f35f1cfdea039246d77bf
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-02-27 08:21:03 -0600

    0.1.3: Heartbeat Requeue Update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="conplyent",
-    version="0.1.2",
+    version="0.1.3",
     license="MIT",
     author="Jayesh Joshi",
     author_email="jayeshjo1@utexas.edu",
