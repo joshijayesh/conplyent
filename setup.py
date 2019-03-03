839eaf608c3b9f1059017afef4be3dd5e21be30c
Jayesh Joshi <jayeshjo1@utexas.edu>
2019-03-03 10:05:06 -0600

    Update to 0.2.0: logging update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="conplyent",
-    version="0.1.5",
+    version="0.2.0",
     license="MIT",
     author="Jayesh Joshi",
     author_email="jayeshjo1@utexas.edu",
